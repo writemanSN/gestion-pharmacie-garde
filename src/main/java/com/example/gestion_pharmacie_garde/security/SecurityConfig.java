33f6bb71750ee59667a25078d8d5768b89fad596
writemanSN <writeman246@gmail.com>
2025-12-30 18:52:48 +0100

    Premier commit du projet pharmacie de garde

--- src/main/java/com/example/gestion_pharmacie_garde/security/SecurityConfig.java
+++ src/main/java/com/example/gestion_pharmacie_garde/security/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/",
+                                "/logo.png",
                                 "/index.html",
                                 "/js/**",
                                 "/css/**",
